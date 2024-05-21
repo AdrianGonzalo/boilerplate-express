e54d0e45a79fd08d4500827ad25a8aa17796e17f
Adrian Martin <adriangonzaloma@gmail.com>
2024-05-21 10:52:22 +0000

    edit

--- myApp.js
+++ myApp.js
@@ -1,10 +1,27 @@
-let express = require('express');
-let app = express();
+// let express = require('express');
+// let app = express();
 
 
+// app.get('/', (req, res) => {
+
+//     res.sendFile(__dirname + "/views/index.html");
+// });
+
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = 3000;
+
+// Configuración de la ruta para enviar el archivo index.html
 app.get('/', (req, res) => {
+  const absolutePath = path.join(__dirname, 'views', 'index.html');
+  res.sendFile(absolutePath);
+});
 
-    res.sendFile(__dirname + "/views/index.html")
+// Iniciar el servidor
+app.listen(port, () => {
+  console.log(`Servidor escuchando en http://localhost:${port}`);
 });
 
 
