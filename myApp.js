8f4e918c19632b3b0fb2fc68c22beb0fccaa699c
Adrian Martin <adriangonzaloma@gmail.com>
2024-05-21 10:53:12 +0000

    edit

--- myApp.js
+++ myApp.js
@@ -2,12 +2,14 @@ let express = require('express');
 let app = express();
 
 
-app.get('/', (req, res) => {
+// app.get('/', (req, res) => {
 
-    res.sendFile(__dirname + "/views/index.html");
-});
+//     res.sendFile(__dirname + "/views/index.html");
+// });
 
-
+app.get("/", function(req, res) {
+    res.sendFile(__dirname + "/public/index.html");
+  });
 
 
 
