1d073f5cfc9b70b92dcaf150a15eed98619fe870
Adrian Martin <adriangonzaloma@gmail.com>
2024-05-21 10:20:16 +0000

    add get

--- myApp.js
+++ myApp.js
@@ -1,9 +1,13 @@
 let express = require('express');
 let app = express();
 
-app.get('/', function(req, res) {
+app.get("/", (req, res) => {
     res.send('Hello Express')
-})
+});
+
+app.get("/", function(req, res) {
+    res.send('Hello Express')
+});
 
 console.log('Hello World')
 
